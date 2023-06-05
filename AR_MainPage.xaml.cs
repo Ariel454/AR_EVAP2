00bd4930d0a2744f42901a9f63e7512bc27f5a20
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-06-05 08:59:28 -0500

    ca

--- AR_MainPage.xaml.cs
+++ AR_MainPage.xaml.cs
@@ -2,9 +2,9 @@ namespace AR_EVAP2;
 
 public partial class AR_MainPage : ContentPage
 {
-	public AR_MainPage()
-	{
-		InitializeComponent();
+    public AR_MainPage()
+    {
+        InitializeComponent();
 
         List<string> elementos = new List<string>
         {
@@ -14,6 +14,22 @@ public partial class AR_MainPage : ContentPage
         };
 
         ElementPicker.ItemsSource = elementos;
+        RadioButton1.CheckedChanged += RadioButton_CheckedChanged;
+        RadioButton2.CheckedChanged += RadioButton_CheckedChanged;
+        RadioButton3.CheckedChanged += RadioButton_CheckedChanged;
+
+    }
+
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is RadioButton radioButton && radioButton.IsChecked)
+        {
+            TextoMostrarLabel.Text = ((StackLayout)radioButton.Parent).Children
+                .OfType<Label>()
+                .FirstOrDefault()
+                ?.Text;
+        }
     }
 
     private async void LearnMore_Clicked(object sender, EventArgs e)
@@ -25,12 +41,6 @@ public partial class AR_MainPage : ContentPage
     private void OnButtonClicked(object sender, EventArgs e)
     {
         ElementPicker.Focus();
-    }
-
-    private void OnPickerSelectionChanged(object sender, EventArgs e)
-    {
-        var selectedElement = ElementPicker.SelectedItem as string;
-        ElementEntry.Text = selectedElement;
     }
 
 
